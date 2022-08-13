defa73ed0edd83cb924c0f234126b9f5cd14c371
Joao Lima <joaovitorsoareslima12@gmail.com>
2022-08-13 15:21:05 -0300

    CRUD de tarefas finalizado

--- Tarefa/Controllers/HomeController.cs
+++ Tarefa/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tarefa.Data;
+using Tarefa.Model;
 
 namespace Tarefa.Controllers
 {
@@ -6,9 +8,55 @@ namespace Tarefa.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public string Get()
+        public IActionResult Buscar([FromServices] AppDbContext context)
+            => Ok(context.Tarefas.ToList());
+
+        [HttpGet("/{id:int}")]
+        public IActionResult BuscarPorId([FromServices] AppDbContext context, [FromRoute] int id)
         {
-            return "Hello dotnet 6";
+            var tarefas = context.Tarefas.FirstOrDefault(x => x.Id == id);
+            if (tarefas == null)
+                return NotFound();
+
+            return Ok(tarefas);
+        }
+
+        [HttpPost("/CriarTarefa")]
+        public IActionResult Criar([FromServices] AppDbContext context, [FromBody] TarefaModel tarefa)
+        {
+            context.Tarefas.Add(tarefa);
+            context.SaveChanges();
+
+            return Created($"/{tarefa.Id}", tarefa);
+        }
+
+        [HttpPut("/AtualizarTarefa")]
+        public IActionResult Atualizar([FromServices] AppDbContext context, [FromBody] TarefaModel tarefa)
+        {
+            var model = context.Tarefas.FirstOrDefault(x => x.Id == tarefa.Id);
+            if (model == null)
+                return NotFound();
+
+            model.Titulo = tarefa.Titulo;
+            model.Finalizada = tarefa.Finalizada;
+
+            context.Tarefas.Update(model);
+            context.SaveChanges();
+
+            return Ok(model);
+        }
+
+        [HttpDelete("/DeletarTarefa/{id:int}")]
+        public IActionResult Detelar([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            var model = context.Tarefas.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return NotFound();
+
+            context.Tarefas.Remove(model);
+            context.SaveChanges();
+
+            return Ok(model);
         }
     }
 }
--- Tarefa/Program.cs
+++ Tarefa/Program.cs
@@ -1,7 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-var app = builder.Build();
+using Tarefa.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+//addicionando o serviço de controler
+builder.Services.AddControllers();
+//criando o dbcontxt como serviço, assim o aspnet gerencia o dispose do objeto
+builder.Services.AddDbContext<AppDbContext>();
+var app = builder.Build();
+//mapenado todas as controllers
 app.MapControllers();
 
 
