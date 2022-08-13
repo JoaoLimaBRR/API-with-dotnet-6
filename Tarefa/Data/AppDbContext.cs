1d4ac0255e0bbdf64a2c42b130c804d286178ae4
Joao Lima <joaovitorsoareslima12@gmail.com>
2022-08-13 14:19:09 -0300

    Entrega inicial

---
+++ Tarefa/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarefa.Model;
+
+namespace Tarefa.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Datasource=app.db;Cache=Shared");
+
+
+    }
+}
---
+++ Tarefa/Model/TarefaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarefa.Model
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public bool Finalizada { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
